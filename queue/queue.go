b7a7bb2494d8a86c581c605fcc0b228495d3e338
gouravk3 <devgouravk3@gmail.com>
2024-12-25 19:34:06 +0530

    queue implementation in golang

---
+++ queue/queue.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	q := new(Queue)
+	q.enqueue(1)
+	q.enqueue(2)
+	q.enqueue(3)
+	q.enqueue(4)
+
+	fmt.Printf("q.pop(): %v\n", q.dequeue())
+	fmt.Printf("q.len(): %v\n", q.len())
+	fmt.Printf("q: %v\n", *q)
+	fmt.Printf("q.peek(): %v\n", q.peek())
+}
+
+type Queue []int
+
+func (q *Queue) dequeue() int {
+	ret := (*q)[0]
+	(*q) = (*q)[1:]
+	return ret
+}
+
+func (q *Queue) enqueue(n int) {
+	*q = append(*q, n)
+}
+
+func (q *Queue) peek() int {
+	return (*q)[0]
+}
+
+func (q *Queue) len() int {
+	return len(*q)
+}
